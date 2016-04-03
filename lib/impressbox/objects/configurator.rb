07008f9fae700632160247373fa5f702bacba2cb
Raimondas Rimkevičius <github@mekdrop.name>
2016-04-04 02:22:00 +0300

    rubocop work

--- lib/impressbox/objects/configurator.rb
+++ lib/impressbox/objects/configurator.rb
@@ -78,31 +78,31 @@ module Impressbox
 
       def insert_ssh_key_if_needed(public_key, private_key)
         @machine.communicate.wait_for_ready 60
-        
+
         machine_private_key @machine.communicate, private_key
         machine_public_key @machine.communicate, public_key
       end
-      
+
       def machine_public_key(communicator, public_key)
-        puts "Updating public key..."
-        machine_upload_file communicator, public_key, '~/.ssh/id_rsa.pub'        
-        communicator.execute "touch ~/.ssh/authorized_keys"
-        communicator.execute "cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
+        puts 'Updating public key...'
+        machine_upload_file communicator, public_key, '~/.ssh/id_rsa.pub'
+        communicator.execute 'touch ~/.ssh/authorized_keys'
+        communicator.execute 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys'
         communicator.execute "echo `awk '!a[$0]++' ~/.ssh/authorized_keys` > ~/.ssh/authorized_keys"
-        communicator.execute "chmod 600 ~/.ssh/id_rsa.pub"
+        communicator.execute 'chmod 600 ~/.ssh/id_rsa.pub'
       end
-      
+
       def machine_private_key(communicator, private_key)
-        puts "Updating private key..."
+        puts 'Updating private key...'
         machine_upload_file communicator, private_key, '~/.ssh/id_rsa'
-        communicator.execute "chmod 400 ~/.ssh/id_rsa"
-      end    
-      
+        communicator.execute 'chmod 400 ~/.ssh/id_rsa'
+      end
+
       def machine_upload_file(communicator, src_file, dst_file)
         communicator.execute 'chmod 777 ' + dst_file + ' || :'
         communicator.execute 'touch ' + dst_file
-        communicator.execute 'truncate -s 0 ' + dst_file        
-        text=File.open(src_file).read
+        communicator.execute 'truncate -s 0 ' + dst_file
+        text = File.open(src_file).read
         text.gsub!(/\r\n?/, "\n")
         text.each_line do |line|
           communicator.execute "echo \"#{line.rstrip}\" >> #{dst_file}"
@@ -120,7 +120,7 @@ module Impressbox
           end
         end
       end
-      
+
       # Configure network
       def configure_network(ip)
         return unless ip
