0022dc6c7b73052b3dd066d55d34f14fc85a46d5
Raimondas Rimkevičius <github@mekdrop.name>
2016-05-30 10:49:40 +0300

    added comments

--- lib/vagrant-impressbox/configurators/abstract_primary.rb
+++ lib/vagrant-impressbox/configurators/abstract_primary.rb
@@ -2,13 +2,16 @@ module Impressbox
   module Configurators
       # This is a base to use for other default configurators
       class AbstractPrimary
+        # This method is used to configure/run configurator
         def configure(_vagrant_config, _config_file)
           raise I18n.t('configuring.error.must_overwrite')
         end
 
+        # This method is used for description
         def description
         end
 
+        # Is method validates if can be executed
         def can_be_configured?(_vagrant_config, _file_config)
           true
         end
--- lib/vagrant-impressbox/configurators/abstract_provision.rb
+++ lib/vagrant-impressbox/configurators/abstract_provision.rb
@@ -3,13 +3,16 @@ module Impressbox
   module Configurators
       # Base class used for provision tasks
       class AbstractProvision
+        # This method is used to configure/run configurator
         def configure(_machine, _config_file)
           raise I18n.t('configuring.error.must_overwrite')
         end
 
+        # This method is used for description
         def description
         end
 
+        # Is method validates if can be executed
         def can_be_configured?(_machine, _config_file)
           true
         end
